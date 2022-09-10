455ea1834a266659e28a35698f39dff28dfa76a6
pumbas600 <joshjeffers600@gmail.com>
2022-09-10 20:47:32 +1200

    feat: complete creating new job functionality

--- src/components/jobs/NewJob.tsx
+++ src/components/jobs/NewJob.tsx
@@ -1,30 +1,45 @@
-import { Button, FormControl, FormErrorMessage, FormLabel, Input, Stack, Textarea } from '@chakra-ui/react';
+import { Button, FormControl, FormErrorMessage, FormLabel, Input, Select, Stack, Textarea } from '@chakra-ui/react';
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllClients } from '../../redux/slices/ClientManager.slice';
+import { addJob } from '../../redux/slices/JobManager.slice';
 import { Status } from '../../types/JobInfo';
 import NoteInfo from '../../types/NoteInfo';
 import Notes from '../notes';
 import StatusMenu from '../status/StatusMenu';
 
 interface FormValidations {
-    title?: string;
+    name?: string;
+    client?: string;
 }
 
-const NewJob = ({ handleDiscard }: { handleDiscard(): void }) => {
-    const [title, setTitle] = useState('');
+const NewJob = ({ closeNewJob }: { closeNewJob(): void }) => {
+    const dispatch = useDispatch();
+    const allClients = useSelector(getAllClients);
+
+    const [name, setName] = useState('');
     const [status, setStatus] = useState(Status.Scheduled);
+    const [client, setClient] = useState<string>('');
     const [description, setDescription] = useState('');
     const [notes, setNotes] = useState<NoteInfo[]>([]);
     const [validations, setValidations] = useState<FormValidations>({});
 
-    const handleChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setTitle(e.target.value);
-        if (validations.title) {
-            setValidations({ ...validations, title: undefined });
+    const handleChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setName(e.target.value);
+        if (validations.name) {
+            setValidations({ ...validations, name: undefined });
         }
     };
 
     const handleChangeStatus = (newStatus: Status) => {
         setStatus(newStatus);
+    };
+
+    const handleChangeClient = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setClient(e.target.value);
+        if (validations.client && allClients[e.target.value] !== undefined) {
+            setValidations({ ...validations, client: undefined });
+        }
     };
 
     const handleChangeDescription = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -45,38 +60,60 @@ const NewJob = ({ handleDiscard }: { handleDiscard(): void }) => {
     };
 
     const handleSave = () => {
-        if (title.length === 0) {
-            setValidations({ ...validations, title: 'A job title is required' });
+        let isValid = true;
+        const validations: FormValidations = {};
+        if (name.length === 0) {
+            validations.name = 'A job title is required';
+            isValid = false;
+        }
+        if (allClients[client] === undefined) {
+            validations.client = 'You must select a client';
+            isValid = false;
+        }
+
+        if (isValid) {
+            dispatch(addJob({ name, status, client: allClients[client], description, notes }));
+        } else {
+            setValidations(validations);
         }
     };
 
     return (
         <Stack px={2} w="full" spacing={4}>
-            <FormControl isRequired isInvalid={validations.title !== undefined}>
-                <FormLabel>Title</FormLabel>
+            <FormControl isRequired isInvalid={validations.name !== undefined}>
+                <Stack>
+                    <FormLabel>Name</FormLabel>
+                </Stack>
                 <Input
                     variant="flushed"
                     fontSize="2xl"
                     fontWeight="bold"
                     color="blue.600"
-                    placeholder="Job Title..."
-                    value={title}
-                    onChange={handleChangeTitle}
+                    placeholder="Job Name..."
+                    value={name}
+                    onChange={handleChangeName}
                 />
-                <FormErrorMessage>{validations.title}</FormErrorMessage>
+                <FormErrorMessage>{validations.name}</FormErrorMessage>
             </FormControl>
             <FormControl isRequired>
                 <FormLabel>Status</FormLabel>
                 <StatusMenu status={status} handleChange={handleChangeStatus} />
             </FormControl>
-            <FormControl isRequired>
+            <FormControl isRequired isInvalid={validations.client !== undefined}>
                 <FormLabel>Client</FormLabel>
-                <StatusMenu status={status} handleChange={handleChangeStatus} />
+                <Select value={client} placeholder="Select client..." onChange={handleChangeClient}>
+                    {Object.values(allClients).map((client) => (
+                        <option key={client.id} value={client.id}>
+                            {client.name}
+                        </option>
+                    ))}
+                </Select>
+                <FormErrorMessage>{validations.client}</FormErrorMessage>
             </FormControl>
             <FormControl>
                 <FormLabel>Job Description</FormLabel>
                 <Textarea
-                    placeholder="This job must be done by..."
+                    placeholder="Add some more information about the job here..."
                     value={description}
                     onChange={handleChangeDescription}
                 />
@@ -87,8 +124,8 @@ const NewJob = ({ handleDiscard }: { handleDiscard(): void }) => {
                 handleUpdate={handleUpdateNote}
                 handleDelete={handleDeleteNote}
             />
-            <Stack direction="row" w="full" pt={5}>
-                <Button variant="outline" colorScheme="blue" w="full" onClick={handleDiscard}>
+            <Stack direction="row" w="full">
+                <Button variant="outline" colorScheme="blue" w="full" onClick={closeNewJob}>
                     Discard
                 </Button>
                 <Button type="submit" colorScheme="blue" w="full" onClick={handleSave}>
